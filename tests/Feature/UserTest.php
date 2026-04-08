0f8a9762b177115d05c75f5563ee8279cd33d175
Thiago Leite <thiagoleites@gmail.com>
2026-04-08 01:58:39 -0300

    test(users): add tests for user creation, roles and active status

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -19,3 +19,15 @@ it('creates an inactive user', function() {
 
     expect($user->is_active)->toBeFalse();
 });
+
+
+it('does not allow duplicate emails', function() {
+
+    User::factory()->create([
+        'email' => 'test@email.com'
+    ]);
+
+    User::factory()->create([
+        'email' => 'test@email.com'
+    ]);
+})->throws(\Illuminate\Database\QueryException::class);
